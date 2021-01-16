89434626b7807306b86d3ebec7085663e1737003
jojowither <ms0421555@gmail.com>
2021-01-16 15:39:19 +0800

    upload get_stock_info

---
+++ src/get_stock_info.py
@@ -0,0 +1,28 @@
+import twstock
+import pandas as pd
+
+def main():
+    all_twstock = twstock.codes
+    all_twstock_code = list(all_twstock.keys())
+    all_twstock_type = []
+    all_twstock_name = []
+    all_twstock_group = []
+    all_twstock_market = []
+
+    for twstock_code in all_twstock_code:
+        all_twstock_type.append(all_twstock[twstock_code].type)
+        all_twstock_name.append(all_twstock[twstock_code].name)
+        all_twstock_group.append(all_twstock[twstock_code].group)
+        all_twstock_market.append(all_twstock[twstock_code].market)
+
+    stock_df = pd.DataFrame({'code': all_twstock_code,
+                            'type': all_twstock_type,
+                            'name': all_twstock_name,
+                            'group': all_twstock_group,
+                            'market': all_twstock_market,})
+
+    stock_df.to_csv('tw_stock_info.csv', encoding='utf-8')
+
+
+if __name__ == "__main__":
+    main()
