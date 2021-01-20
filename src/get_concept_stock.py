0c6e8981dfe5f7d1cb7f41c2d6a958ea66f705fa
jojowither <ms0421555@gmail.com>
2021-01-20 13:57:43 +0800

    done

--- src/get_concept_stock.py
+++ src/get_concept_stock.py
@@ -1,9 +1,12 @@
+import re
+import time
+
 import pandas as pd
 import twstock
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
 from tqdm import tqdm
-import re
 
 CHROMEDRIVER_PATH = '/usr/local/bin/chromedriver'
 # pchome股市概念股
@@ -13,30 +16,36 @@ ALL_CONCEPT_XPATH = '//*[@id="bttb"]/table/tbody/tr/td/div[2]'
 SELECT_XPATH = '//*[@id="bttb"]/div[5]/select'
 INDIV_CONCEPT_XPATH = '//*[@id="bttb"]/table[3]'
 
+
 def init_driver():
     # initialize driver
     options = webdriver.ChromeOptions()
     options.add_argument("headless")
     options.add_argument("--disable-notifications")
     driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=options)
+    driver.get(CONCEPT_STCOK_URL)
     return driver
 
+
 def get_all_concept(driver):
-    driver.get(CONCEPT_STCOK_URL)
     concepts = driver.find_element_by_xpath(ALL_CONCEPT_XPATH)
     concepts = concepts.text.split('\n')
     return concepts
 
-def get_all_subpages(driver):
+
+def get_all_subpages(driver, concepts_xpath):
     select = Select(driver.find_element_by_xpath(SELECT_XPATH))
     subpages = [opt.text for opt in select.options]
     return select, subpages
 
+
 def get_stock_table(driver):
-    indiv_concept_table = driver.find_element_by_xpath(INDIV_CONCEPT_XPATH).get_attribute('outerHTML')
+    indiv_concept_table = driver.find_element_by_xpath(
+        INDIV_CONCEPT_XPATH).get_attribute('outerHTML')
     indiv_concept_table = pd.read_html(indiv_concept_table)[0]
     stocks = indiv_concept_table['股票▲'].tolist()
     return stocks
+
 
 def construct_concept_df(stocks, concept, concept_df):
     for stock in stocks:
@@ -46,8 +55,9 @@ def construct_concept_df(stocks, concept, concept_df):
         concept_df = concept_df.append(concept_dict, ignore_index=True)
     return concept_df
 
+
 def filter_code(stock):
-    pattern = r"\(\d+\)"
+    pattern = r"\(\d+\w*\)"
     stock_code = re.search(pattern, stock).group()
     stock_code = re.sub(r'[\(\)]', "", stock_code)
     return stock_code
@@ -63,21 +73,28 @@ def main():
                                                colour='#00af91'), start=1):
         # click in to concept
         concepts_xpath = f'{ALL_CONCEPT_XPATH}/span[{concept_idx}]/a'
-        driver.find_element_by_xpath(concepts_xpath).click()
+        cur_url = driver.current_url
+        while True:
+            driver.find_element_by_xpath(concepts_xpath).click()
+            if driver.current_url != cur_url:
+                break
 
-        select, subpages = get_all_subpages(driver)
+        select, subpages = get_all_subpages(driver, concepts_xpath)
         for page_idx, opt in enumerate(subpages):
-            if page_idx!=0:
+            if page_idx != 0:
                 select.select_by_value(opt)
+                select = Select(driver.find_element_by_xpath(SELECT_XPATH))
+                # time.sleep(5)
 
             stocks = get_stock_table(driver)
             concept_df = construct_concept_df(stocks, concept, concept_df)
-            
+
         # driver.execute_script("window.history.go(-1)")
         driver.get(CONCEPT_STCOK_URL)
 
     concept_df.to_csv('../data/concept_prep.csv', encoding='utf-8')
     print('\nDone')
+
 
 if __name__ == "__main__":
     main()
